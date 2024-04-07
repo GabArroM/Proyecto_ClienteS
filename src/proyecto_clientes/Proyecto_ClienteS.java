283564a1ee0835001ef624527e726fed555fdef9
GabArroM <gaboarma1@gmail.com>
2024-04-06 18:29:38 -0600

    prueba gabriel

--- src/proyecto_clientes/Proyecto_ClienteS.java
+++ src/proyecto_clientes/Proyecto_ClienteS.java
@@ -14,7 +14,7 @@ public class Proyecto_ClienteS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
