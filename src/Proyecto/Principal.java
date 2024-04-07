0df932d1e4173cdf550d104a6c47aaf402d45b2d
kalmacr <166265026+kalmacr@users.noreply.github.com>
2024-04-06 19:01:27 -0600

    Update Principal.java

--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package Proyecto;
 
 /**
@@ -14,7 +11,7 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        System.out.println("hola mundo"); 
     }
     
 }
