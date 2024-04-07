c715d5a5a745ed4fb8425633dabd69eb4a62db9f
tomesme <tomesme04@gmail.com>
2024-04-06 20:58:19 -0600

    Main de hilos
    Problema con el push

--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -5,14 +5,25 @@ package Proyecto;
  *
  * @author Usuario
  */
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        System.out.println("Prueba uno"); 
-        System.out.println("Prueba dos");
+        // Creamos una instancia de Parqueo con 20 espacios
+        Parqueo parqueo = new Parqueo(20);
+
+        // Creamos un ExecutorService con 10 hilos (simula 10 carros llegando al parqueo simultáneamente)
+        ExecutorService ejecutor = Executors.newFixedThreadPool(10);
+
+        // Simulamos la llegada de 10 carros
+        for (int i = 1; i <= 10; i++) {
+            ejecutor.execute(new Carro(i, parqueo));
+        }
+
+        // Terminamos el ExecutorService
+        ejecutor.shutdown();
+        while (!ejecutor.isTerminated()) ;
     }
-    
 }
