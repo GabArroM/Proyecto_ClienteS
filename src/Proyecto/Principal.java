58861b18256ea845244d5a5bb1549773570a9e4e
GabArroM <gaboarma1@gmail.com>
2024-04-06 18:47:06 -0600

    cambio de nombres

--- src/Proyecto/Principal.java
+++ src/Proyecto/Principal.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package proyecto_clientes;
+package Proyecto;
 
 /**
  *
  * @author Usuario
  */
-public class Proyecto_ClienteS {
+public class Principal {
 
     /**
      * @param args the command line arguments
